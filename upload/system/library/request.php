ab5c375a483509e3948bd6a555c31d25c027ae22
Esmond <esmond@equotix.com>
2015-09-16 21:47:54 +0800

    Revert "preparing for new functions"
    This reverts commit 3efd79cb0286b4f79dff5aa442603a06c2869871.

--- upload/system/library/request.php
+++ upload/system/library/request.php
@@ -1,24 +1,21 @@
 <?php
 class Request {
-	public $cookie = array();
-	public $files = array();
     public $get = array();
     public $post = array();
-    public $request = array();
+    public $cookie = array();
+    public $files = array();
     public $server = array();
-	public $session = array();
 
     public function __construct() {
-		$this->cookie = $this->clean($_COOKIE);
-		$this->files = $this->clean($_FILES);
         $this->get = $this->clean($_GET);
         $this->post = $this->clean($_POST);
         $this->request = $this->clean($_REQUEST);
+        $this->cookie = $this->clean($_COOKIE);
+        $this->files = $this->clean($_FILES);
         $this->server = $this->clean($_SERVER);
-		$this->session = $this->clean($_SESSION);
     }
 
-    private function clean($data) {
+    public function clean($data) {
         if (is_array($data)) {
             foreach ($data as $key => $value) {
                 unset($data[$key]);
@@ -31,60 +28,4 @@ class Request {
 
         return $data;
     }
-	
-	public function cookie($key = false) {
-		if ($key) {
-			return isset($this->cookie[$key]) ? $this->cookie[$key] : false;
-		} else {
-			return $this->cookie;
-		}
-	}
-	
-	public function files($key = false) {
-		if ($key) {
-			return isset($this->files[$key]) ? $this->files[$key] : false;
-		} else {
-			return $this->files;
-		}
-	}
-	
-	public function get($key = false) {
-		if ($key) {
-			return isset($this->get[$key]) ? $this->get[$key] : false;
-		} else {
-			return $this->request->get;
-		}
-	}
-	
-	public function post($key = false) {
-		if ($key) {
-			return isset($this->post[$key]) ? $this->post[$key] : false;
-		} else {
-			return $this->request->post;
-		}
-	}
-	
-	public function request($key = false) {
-		if ($key) {
-		return isset($this->request[$key]) ? $this->request[$key] : false;
-		} else {
-			return $this->request;
-		}
-	}
-	
-	public function server($key = false) {
-		if ($key) {
-			return isset($this->server[$key]) ? $this->server[$key] : false;
-		} else {
-			return $this->server;
-		}
-	}
-	
-	public function session($key = false) {
-		if ($key) {
-			return isset($this->session[$key]) ? $this->session[$key] : false;
-		} else {
-			return $this->session;
-		}
-	}
 }
