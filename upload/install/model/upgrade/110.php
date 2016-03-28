936607b299c36f331274d52163bd9bce728130fb
Esmond <esmond@equotix.com>
2016-03-28 10:23:49 +0800

    Database upgrade for next release

--- upload/install/model/upgrade/110.php
+++ upload/install/model/upgrade/110.php
@@ -1,5 +1,6 @@
 <?php
 class ModelUpgrade110 extends Model {
+	// Upgrading to Logic Invoice version 1.1.0 database
     public function upgrade($db) {
         // Update serialized settings data to json
 		$query = $db->query("SELECT * FROM " . DB_PREFIX . "setting WHERE serialized = '1'");
@@ -22,5 +23,21 @@ class ModelUpgrade110 extends Model {
 				$db->query("UPDATE " . DB_PREFIX . "user_group SET permission = '" . $db->escape(json_encode($permission)) . "' WHERE user_group_id = '" . (int)$result['user_group_id'] . "'");
 			}
 		}
+		
+		// Add new database tables
+		$this->db->query("CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "inventory` (
+		  `inventory_id` int(11) NOT NULL AUTO_INCREMENT,
+		  `sku` varchar(255) NOT NULL,
+		  `name` varchar(255) NOT NULL,
+		  `description` text NOT NULL,
+		  `image` varchar(255) NOT NULL,
+		  `quantity` int(11) NOT NULL,
+		  `cost` decimal(15,4) NOT NULL,
+		  `sell` decimal(15,4) NOT NULL,
+		  `status` tinyint(1) NOT NULL,
+		  `date_added` datetime NOT NULL,
+		  `date_modified` datetime NOT NULL,
+		  PRIMARY KEY (`inventory_id`)
+		) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;");
     }
 }
