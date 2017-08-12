355293029a2cfdf9421f61f7bec5835d47fe2501
Dimitris Zervas <dzervas@dzervas.gr>
2017-08-13 00:22:11 +0300

    Typo in config-prod.php

--- config-prod.php
+++ config-prod.php
@@ -5,7 +5,7 @@ define('APP_SURL', getenv('LI_SURL'));
 
 // Database
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'database');
+define('DB_HOSTNAME', 'db');
 define('DB_USERNAME', getenv('MYSQL_USER'));
 define('DB_PASSWORD', getenv('MYSQL_PASSWORD'));
 define('DB_DATABASE', getenv('MYSQL_DATABASE'));
